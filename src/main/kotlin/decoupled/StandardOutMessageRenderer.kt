967b4ae635548eac42c2e3235dea54a684172fea
arisromil <arisromil@yahoo.com>
2023-09-21 09:59:53 -0600

    Create StandardOutMessageRenderer.kt
    MessageRenderer Implementation

---
+++ src/main/kotlin/decoupled/StandardOutMessageRenderer.kt
@@ -0,0 +1,22 @@
+package decoupled
+
+class StandardOutMessageRenderer : MessageRenderer {
+    override var messageProvider: MessageProvider? = null
+        set(value) {
+            field = value
+            println(" --> StandardOutMessageRenderer: setting the provider")
+        }
+
+    init {
+        println(" --> StandardOutMessageRenderer: constructor called")
+    }
+
+    override fun render() {
+        println(
+            messageProvider?.message ?: throw RuntimeException(
+                "You must set the property messageProvider of class:"
+                        + StandardOutMessageRenderer::class.java.name
+            )
+        )
+    }
+}
